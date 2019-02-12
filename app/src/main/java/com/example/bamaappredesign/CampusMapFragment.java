c3fd6acfb1cad684c987a682442d8004a9895c17
Ethan Johnson <ekjohnson4@crimson.ua.edu>
2019-02-12 17:15:23 -0600

    Fixed issue where map did not fill screen.

--- app/src/main/java/com/example/bamaappredesign/CampusMapFragment.java
+++ app/src/main/java/com/example/bamaappredesign/CampusMapFragment.java
@@ -1,6 +1,5 @@
 package com.example.bamaappredesign;
 
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,7 +7,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
-import android.widget.ProgressBar;
 
 public class CampusMapFragment extends Fragment {
     private WebView mWebView;
