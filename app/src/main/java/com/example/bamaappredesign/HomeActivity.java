f7a49f21a09b9d6a304256263fc2b31a8ef49d2e
Ethan Johnson <ekjohnson4@crimson.ua.edu>
2019-02-17 17:01:03 -0600

    Created pages that branch off of Student

--- app/src/main/java/com/example/bamaappredesign/HomeActivity.java
+++ app/src/main/java/com/example/bamaappredesign/HomeActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.*;
 import android.support.v4.app.FragmentTransaction;
-import android.view.View;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -141,13 +140,11 @@ public class HomeActivity extends AppCompatActivity
                 Intent i = new Intent(getApplicationContext(), HomeActivity.class);
                 startActivity(i);
             }
-
         }
         else if (id == R.id.map) {
             FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
             ft.replace(R.id.flMain, new CampusMapFragment());
             ft.commit();
-
         }
         else if (id == R.id.student) {
             FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
--- app/src/main/java/com/example/bamaappredesign/StudentFragment.java
+++ app/src/main/java/com/example/bamaappredesign/StudentFragment.java
@@ -1,29 +1,75 @@
 package com.example.bamaappredesign;
 
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-
-/**
- * A simple {@link Fragment} subclass.
- */
 public class StudentFragment extends Fragment {
-
 
     public StudentFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_student, container, false);
-    }
+       View inputView =  inflater.inflate(R.layout.fragment_student,
+                container,
+                false
+        );
 
+        //Grades button
+        Button grades = inputView.findViewById(R.id.grades_button);
+        View.OnClickListener gradesListener = new View.OnClickListener() {
+            @Override
+            public void onClick(android.view.View view) {
+                FragmentTransaction ft = getFragmentManager().beginTransaction();
+                ft.replace(R.id.flMain, new GradesFragment());
+                ft.commit();
+            }
+        };
+
+        //Schedule button
+        Button schedule = inputView.findViewById(R.id.schedule_button);
+        View.OnClickListener scheduleListener = new View.OnClickListener() {
+            @Override
+            public void onClick(android.view.View view) {
+                FragmentTransaction ft = getFragmentManager().beginTransaction();
+                ft.replace(R.id.flMain, new ScheduleFragment());
+                ft.commit();
+            }
+        };
+
+        //MyTickets button
+        Button tickets = inputView.findViewById(R.id.tickets_button);
+        View.OnClickListener ticketsListener = new View.OnClickListener() {
+            @Override
+            public void onClick(android.view.View view) {
+                FragmentTransaction ft = getFragmentManager().beginTransaction();
+                ft.replace(R.id.flMain, new TicketsFragment());
+                ft.commit();
+            }
+        };
+
+        //Action Card button
+        Button card = inputView.findViewById(R.id.action_card_button);
+        View.OnClickListener cardListener = new View.OnClickListener() {
+            @Override
+            public void onClick(android.view.View view) {
+                FragmentTransaction ft = getFragmentManager().beginTransaction();
+                ft.replace(R.id.flMain, new ActionCardFragment());
+                ft.commit();
+            }
+        };
+
+        grades.setOnClickListener(gradesListener);
+        schedule.setOnClickListener(scheduleListener);
+        tickets.setOnClickListener(ticketsListener);
+        card.setOnClickListener(cardListener);
+        return inputView;
+    }
 }
