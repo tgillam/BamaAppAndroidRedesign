238242077ea887289298ec112a867925d201035f
tgillam <tgillam12@gmail.com>
2019-02-17 15:45:11 -0600

    Transportation Page finished

--- app/src/main/java/com/example/bamaappredesign/LaundryFragment.java
+++ app/src/main/java/com/example/bamaappredesign/LaundryFragment.java
@@ -6,12 +6,23 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import java.io.InputStream;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class LaundryFragment extends Fragment {
+
 
 
     public LaundryFragment() {
--- app/src/main/java/com/example/bamaappredesign/TransportationFragment.java
+++ app/src/main/java/com/example/bamaappredesign/TransportationFragment.java
@@ -6,13 +6,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class TransportationFragment extends Fragment {
-
+    private WebView mWebView;
 
     public TransportationFragment() {
         // Required empty public constructor
@@ -23,7 +25,14 @@ public class TransportationFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_transportation, container, false);
+        View view =  inflater.inflate(R.layout.fragment_campus_map, container, false);
+        mWebView = view.findViewById(R.id.campus_map_webview);
+
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        mWebView.loadUrl("https://ua.transloc.com/m/");
+
+        return view;
     }
 
 }
