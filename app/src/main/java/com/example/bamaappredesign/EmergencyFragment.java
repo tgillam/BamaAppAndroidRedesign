f68e116af607e9afbf597fd134f944c0d1efff4f
mjahern <mjahern@crimson.ua.edu>
2019-02-20 05:56:53 -0600

    Start of Emergency, listed all contacts

--- app/src/main/java/com/example/bamaappredesign/EmergencyFragment.java
+++ app/src/main/java/com/example/bamaappredesign/EmergencyFragment.java
@@ -2,28 +2,149 @@ package com.example.bamaappredesign;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+class Contact {
+
+    private String Name;
+    private String Phone;
+
+
+    public Contact() {
+    }
+
+    public Contact(String name, String phone) {//maybe int for phone?
+        Name = name;
+        Phone = phone;
+
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public String getPhone() {
+        return Phone;
+    }
+
+    //settter
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public void setPhone(String phone) {
+        Phone = phone;
+    }
+}
+
+
+
+
+
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class EmergencyFragment extends Fragment {
+public class EmergencyFragment extends Fragment {//implements EmergencyAdapter.ItemClickListener {
+
+    View v;
+    private RecyclerView myrecyclerview;
+    private List<Contact> listContact;
 
 
     public EmergencyFragment() {
         // Required empty public constructor
     }
 
+    //EmergencyAdapter adapter;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_emergency, container, false);
+        v = inflater.inflate(R.layout.fragment_emergency, container, false);
+
+        myrecyclerview = (RecyclerView) v.findViewById(R.id.rvContacts);
+        EmergencyAdapter adapter = new EmergencyAdapter(getContext(),listContact);
+        myrecyclerview.setLayoutManager(new LinearLayoutManager(getActivity()));
+        myrecyclerview.setAdapter(adapter);
+
+        myrecyclerview.addItemDecoration(new DividerItemDecoration(getActivity(), LinearLayoutManager.VERTICAL));
+
+
+        return v;
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        listContact = new ArrayList<>();
+        listContact.add(new Contact("University Police","2053485454"));
+        listContact.add(new Contact("Tuscaloosa Police","2055555543"));
+        listContact.add(new Contact("Tuscaloosa County Sheriff's Office","2055555543"));
+        listContact.add(new Contact("Northport Police","2055555543"));
+        listContact.add(new Contact("348-RIDE (after hours)","2055555543"));
+        listContact.add(new Contact("MAP (business hours)","2055555543"));
+        listContact.add(new Contact("MAP (after hours)","2055555543"));
+        listContact.add(new Contact("University Operator","2055555543"));
+    }
 }
+
+
+
+        /*
+        ArrayList<String> names = new ArrayList<>();
+        names.add("University Police");
+        names.add("Tuscaloosa Police");
+        names.add("Tuscaloosa County Sheriff's Office");
+        names.add("Northport Police");
+        names.add("348-RIDE (after hours)");
+        names.add("MAP (business hours)");
+        names.add("MAP (after hours)");
+        names.add("University Operator");
+
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_emergency, container, false);
+*/
+     /*   RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.rvContacts);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        adapter = new EmergencyAdapter(getActivity(), names);
+
+        //adapter.setClickListener(this);
+        recyclerView.addItemDecoration(new DividerItemDecoration(getActivity(), LinearLayoutManager.VERTICAL));
+
+
+     //   adapter.setClickListener(getActivity());
+
+
+        recyclerView.setAdapter(adapter);
+
+
+        return view;
+    }
+
+   // @Override
+    //public void onItemClick(View view, int position) {
+      //  Toast.makeText(getActivity(), new StringBuilder().append("You clicked ").append(adapter.getItem(position)).append(" on row number ").append(position).toString(), Toast.LENGTH_SHORT).show();
+   //}
+
+}*/
+
+
+
