c2e040a9dafa0c058f160759c93efd365cd4e507
Nicholas Brunelle <45525985+nickbrunelle@users.noreply.github.com>
2019-02-20 22:05:28 -0600

    Action Card Fragment Code

--- app/src/main/java/com/example/bamaappredesign/ActionCardFragment.java
+++ app/src/main/java/com/example/bamaappredesign/ActionCardFragment.java
@@ -1,25 +1,64 @@
 package com.example.bamaappredesign;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-/**
- * A simple {@link Fragment} subclass.
- */
-public class ActionCardFragment extends Fragment {
-
-    public ActionCardFragment() {
-        // Required empty public constructor
+public class ActionCardFragment extends Fragment
+{
+    public ActionCardFragment()
+    {
+        //required empty public constructor
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState)
+    {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_card, container, false);
-    }
+        // return inflater.inflate(R.layout.fragment_card, container, false);
+        View inputView =  inflater.inflate(R.layout.fragment_card,
+                container,
+                false
+        );
 
+         //Bama Cash button
+        Button bamaCash = inputView.findViewById(R.id.bCash_button);
+        View.OnClickListener bCashListener = new View.OnClickListener()
+        {
+            @Override
+            public void onClick(android.view.View view)
+            {
+                assert getFragmentManager() != null;
+                FragmentTransaction fragTran = getFragmentManager().beginTransaction();
+                fragTran.replace(R.id.flMain, new CashFragment());
+                fragTran.addToBackStack(null);
+                fragTran.commit();
+            }
+         };
+
+        //Dining Dollars button
+        Button diningDollars = inputView.findViewById(R.id.dDollars_button);
+        View.OnClickListener dDollarsListener = new View.onClickListener()
+        {
+            @Override
+            public void onClick(android.view.View view)
+            {
+                assert getFragmentManager() != null;
+                FragmentTransaction fragTran = getFragmentManager().beginTransaction();
+                fragTran.replace(R.id.flMain, new DollarFragment());
+                fragTran.addToBackStack(null);
+                fragTran.commit();
+            }
+        };
+
+        bamaCash.setOnClickListener(bCashListener);
+        diningDollars.setOnClickListener(dDollarsListener);
+        return inputView;
+    }
 }
